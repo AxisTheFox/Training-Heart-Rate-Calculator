5f1b7e0b0c6b7e0ca3eba002625fb2cb37300bd4
Fox Braydon <bfox2@csmac5.dhcp.bsu.edu>
2016-10-02 13:57:38 -0400

    Initial project commit.

--- FoxBraydonAssignment2Part1/ViewController.swift
+++ FoxBraydonAssignment2Part1/ViewController.swift
@@ -9,15 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var restingHeartRateTextField: UITextField!
+    @IBOutlet weak var trainingHeartRateLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        ageTextField.text = ""
+        restingHeartRateTextField.text = ""
+        trainingHeartRateLabel.text = ""
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Actions
+    @IBAction func calculateButton(sender: AnyObject) {
+        let age = Double(ageTextField.text!)!
+        let restingHeartRate = Double(restingHeartRateTextField.text!)!
+        
+        let maximumHeartRate = 220 - age
+        
+        let trainingHeartRate = (maximumHeartRate - restingHeartRate) * 0.6 + restingHeartRate
+        trainingHeartRateLabel.text = String(trainingHeartRate)
     }
 
 
